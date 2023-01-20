f2a6988e5b20c81021a9ce1960ac791a937c9bfe
Artem-Kiselev34 <whitesea418@gmail.com>
2023-01-20 13:15:18 +0300

    created section hero and search-form

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,7 @@
 import './components/burger';
+import './components/search-filtr/filtr-dropdown';
+import './components/search-filtr/choosing-city-country';
+import './components/search-filtr/duration';
+import './components/search-filtr/tourists';
+import './components/search-filtr/tourists-children';
+
